845e99a2ae8120d2304e3a46c2b424ddf4448ca1
pablete18 <pabloealcaraz@gmail.com>
2024-07-02 19:59:38 -0300

    mi primer commit

---
+++ holaMundo.ts
@@ -0,0 +1,25 @@
+let mensaje : string = "Hola Mundo"
+
+console.log(mensaje);
+
+
+let i = 1
+for(let i = 1; i<= 10; i++){
+    console.log(i)
+}
+
+let esVerdadero : boolean = true
+console.log('el valor es '+ esVerdadero)
+
+let numero: string= 'hola';
+console.log(numero + 3);
+
+const diasDeSemana = ["Lunes","martes","miercoles","jueves", "viernes","sabado","domingo"];
+
+for(let i = 0; i < diasDeSemana.length; i++){
+    console.log(diasDeSemana[i]);
+    
+}
+
+
+
